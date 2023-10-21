a0674b20ee395c634d436b196995f27d6fe76f31
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-10-21 16:40:27 +0200

    move DB_HOST

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const {DB_HOST} = process.env;
+const {DB_HOST} = require("./config");
 
 mongoose.set("strictQuery", true);
 mongoose
