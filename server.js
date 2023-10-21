9249d851cc16d6789aefc3921cfad19a82c312e3
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-10-21 15:46:15 +0200

    move DB_HOST to config.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Ivan:ejyueCc9vvJYjyp1@cluster0.xt0jebl.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+const {DB_HOST} = process.env
 
 mongoose.set("strictQuery", true);
 mongoose
