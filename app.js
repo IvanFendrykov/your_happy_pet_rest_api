0522f739f1fabf0d8c6e28546805fdd85cea0df8
KolBa228 <technotrashinfo@gmail.com>
2023-11-18 20:29:58 +0000

    update '' in code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
 
-const ourFriendsRouter = require('./routes/api/contacts');
+const ourFriendsRouter = require('./routes/api/ourFriends');
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
@@ -17,11 +17,11 @@ app.use('/api/ourfriends', ourFriendsRouter);
 // app.use();
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
 
