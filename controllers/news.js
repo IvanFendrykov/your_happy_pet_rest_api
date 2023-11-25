9c051e7c4845724b9f0ecb4dbcc2dd6a6448b2eb
VladRaFeed <122728801+VladRaFeed@users.noreply.github.com>
2023-11-25 16:08:15 +0200

    add get request with key word

--- controllers/news.js
+++ controllers/news.js
@@ -1,17 +1,61 @@
 const ctrlWrapper = require('../helpers/ctrlWrapper');
 const News = require("../models/news");
 
+// const getAllNews = ctrlWrapper(async (req, res) => {
+//     // const { page = 1, limit = 6 } = req.query;
+//     // const skip = (page - 1) * limit;
+//     // {}, "", { skip, limit }
+//     const response = await News.find();
+//     const totalPageCount = Math.ceil(response.length / 6);
+    
+//     console.log(totalPageCount)
+
+//     res.status(200).json(response);
+// })
+
 const getAllNews = ctrlWrapper(async (req, res) => {
-    const { page = 1, limit = 6 } = req.query;
+    const { page = 1, limit = 6, } = req.query;
     const skip = (page - 1) * limit;
-    const response = await News.find({}, "", { skip, limit});
-    res.status(200).json({
-        status: 200,
-        statusText: "success",
-        data: {response}
+
+    // Получение общего количества документов в коллекции
+    const totalDocumentsCount = await News.countDocuments({});
+
+    // Расчет общего количества страниц
+    const totalPageCount = Math.ceil(totalDocumentsCount / limit);
+
+    // Получение данных для текущей страницы
+    const response = await News.find({}, "", { skip, limit });
+
+    res.status(200).json({response, totalPageCount});
+});
+
+const getNewsByRequest = ctrlWrapper(async (req, res) => {
+    const { page = 1, limit = 6, } = req.query;
+    const skip = (page - 1) * limit;
+
+    const totalDocumentsCount = await News.countDocuments({
+            "$or": [
+                { title: { $regex: req.params.key } },
+                { text: { $regex: req.params.key } }
+            ]
     });
+
+    const totalPageCount = Math.ceil(totalDocumentsCount / limit);
+    
+    console.log(totalPageCount)
+
+    const response = await News.find(
+        {
+            "$or": [
+                { title: { $regex: req.params.key } },
+                { text: { $regex: req.params.key } }
+            ]
+        }
+    , "" , {skip, limit})
+    res.status(200).json({response, totalPageCount});
 })
 
 module.exports = {
-    getAllNews
+    getAllNews,
+    getNewsByRequest
 }
--- routes/api/news.js
+++ routes/api/news.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const {getAllNews} = require("../../controllers/news");
+const {getAllNews, getNewsByRequest} = require("../../controllers/news");
 
 const router = express.Router();
 
 router.get('/', getAllNews);
+
+router.get('/search/:key', getNewsByRequest)
 
 module.exports = router
