0934b0fce7e3addb352e93d085ecb9bb21b20a32
Pashttett <pavlo3637@gmail.com>
2023-11-26 16:54:20 +0200

    Finally Login
    Add Current, correct(Login, Logout, authenticate)

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { registerSchema, loginSchema } = require('../../models/user');
-const { register, login, logout } = require('../../controllers/users');
-const { authMiddleware } = require('../../middlewares/auth');
+const { register, login,getCurrentUser, logout  } = require('../../controllers/users');
+const { authenticateUser } = require('../../middlewares/auth');
 const { validateBody } = require('../../middlewares');
 
 router.post('/register', validateBody(registerSchema), register);
 router.post('/login', validateBody(loginSchema), login);
-router.post('/logout', authMiddleware, logout);
+router.post('/logout', authenticateUser, logout);
+router.get("/current", authenticateUser, getCurrentUser);
 
 module.exports = router;
