69a0027ee31443938c8d69b51b97e3362ee3c0c1
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-11-26 21:50:53 +0200

    fix: logout and auth

--- controllers/users.js
+++ controllers/users.js
@@ -1,71 +1,70 @@
-const { User } = require('../models/user');
-const { ctrlWrapper, HttpError } = require('../helpers');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const { User } = require("../models/user");
+const { ctrlWrapper, HttpError } = require("../helpers");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
 const register = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (user) {
-        throw HttpError(409, "Email already in use");
-    }
-    
-    const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ ...req.body, password: hashPassword });
-    
-    res.status(201).json({
-        username: newUser.username,
-        email: newUser.email,
-    });
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
+  res.status(201).json({
+    username: newUser.username,
+    email: newUser.email,
+  });
 };
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        throw HttpError(401, "Invalid credentials");
-    }
+  if (!user) {
+    throw HttpError(401, "Invalid credentials");
+  }
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
+  const passwordMatch = await bcrypt.compare(password, user.password);
 
-    if (!passwordMatch) {
-        throw HttpError(401, "Invalid credentials");
-    }
+  if (!passwordMatch) {
+    throw HttpError(401, "Invalid credentials");
+  }
 
-    const payload = {
-        userId: user._id,
-    };
+  const payload = {
+    userId: user._id,
+  };
 
-    const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '24h' });
+  const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "24h" });
 
-    res.status(200).json({ username: user.username, token });
+  await User.findByIdAndUpdate(user._id, { token }, { new: true });
+
+  res.status(200).json({ username: user.username, token });
 };
-
-
 
 const getCurrentUser = async (req, res) => {
   const { user_id, username, email, token, password } = req.body;
 
-  res.json({ user_id, username, email, token, password});
+  res.json({ user_id, username, email, token, password });
 };
 
-
 const logout = async (req, res) => {
-    const { _id } = req.body
-    await User.findByIdAndUpdate(_id, { token: "" });
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
 
-    res.json({
-        message: "No Content"
-    })
+  res.json({
+    message: "No Content",
+  });
 };
 
 module.exports = {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrentUser: ctrlWrapper(getCurrentUser), 
-    logout: ctrlWrapper(logout),
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrentUser: ctrlWrapper(getCurrentUser),
+  logout: ctrlWrapper(logout),
 };
