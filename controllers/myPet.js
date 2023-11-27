c5f16360079143e815b51e42a00b407a70d48664
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-11-27 20:30:12 +0200

    added notices and remove some trash

---
+++ controllers/myPet.js
@@ -0,0 +1,57 @@
+const { HttpError } = require("../helpers");
+const ctrlWrapper = require("../helpers/ctrlWrapper");
+const MyPet = require("../models/myPet");
+const cloudinary = require("../servis/cloudinary");
+
+const addMyPet = ctrlWrapper(async (req, res) => {
+  const { _id: owner } = req.user;
+
+  const { path: tempUpload } = req.file;
+
+  const resultImg = await cloudinary.uploader.upload(tempUpload);
+  const image = resultImg.url;
+  req.body.image = image;
+
+  const response = await MyPet.create({ ...req.body, owner });
+  
+ 
+  res.status(201).json({
+    code: 201,
+    status: "created",
+    data: response,
+  });
+});
+
+const delMyPet = ctrlWrapper(async (req, res) => {
+  const { petId } = req.params;
+  await MyPet.findByIdAndDelete(petId);
+  res.status(200).json({
+    code: 204,
+    status: "Delete success",
+  });
+});
+
+const getAllMyPet = ctrlWrapper(async (req, res) => {
+  const { _id: owner } = req.user;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  const options = {
+    page: page,
+    limit: limit,
+  };
+
+  const response = await MyPet.paginate({ owner }, options);
+
+  res.status(200).json({
+    code: 200,
+    status: "finded",
+    data: { response },
+  });
+});
+
+module.exports = {
+  getAllMyPet,
+  delMyPet,
+  addMyPet,
+};
--- models/myPet.js
+++ models/myPet.js
@@ -1,23 +1,16 @@
 const { Schema, model } = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate");
-
-const LostFoundPetSchema = new Schema({
+const myPetSchema = new Schema({
   name: {
     type: String,
     required: [true, "Please provide the name."],
     minlength: 2,
     maxlength: 20,
   },
-  title: {
-    type: String,
-    required: [true, "Please provide the title."],
-    minlength: 2,
-    maxlength: 20,
-  },
   birthDay: {
     type: Date,
     required: true,
-    match:  /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/,
+    match: /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/,
   },
 
   typeOfPet: {
@@ -26,18 +19,9 @@ const LostFoundPetSchema = new Schema({
     minlength: 2,
     maxlength: 20,
   },
-  gender: {
-    type: String,
-    enum: ["male", "female"],
-    required: [true, 'Please specify the gender as "male" or "female".'],
-  },
   image: {
     type: String,
     required: [true, "Please provide the image URL."],
-  },
-  location: {
-    type: String,
-    required: [true, "Please provide the location."],
   },
   comments: {
     type: String,
@@ -45,13 +29,14 @@ const LostFoundPetSchema = new Schema({
     minlength: 3,
     maxlength: 499,
   },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
 });
-LostFoundPetSchema.plugin(mongoosePaginate);
-const LostFound = model("lost/foundPets", LostFoundPetSchema);
 
-module.exports = LostFound;
+myPetSchema.plugin(mongoosePaginate);
+const MyPet = model("pet", myPetSchema);
 
-
-
-
-
+module.exports = MyPet;
