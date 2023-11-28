af354e06b13329be71edc196637b57513131e8f8
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-11-28 06:59:40 +0200

    fix: isValidId, added: swager=["myPet","nitices"]

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,10 +1,10 @@
-const { isValidObjectId } = require('mongoose');
-const { HttpError } = require('../helpers');
+const { isValidObjectId } = require("mongoose");
+const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
-  const { petId } = req.params;
-  if (!isValidObjectId(petId)) {
-    next(HttpError(400, `${petId} is not valid id`));
+  const { id } = req.params;
+  if (!isValidObjectId(id)) {
+    next(HttpError(400, `${id} is not valid id`));
   }
   next();
 };
